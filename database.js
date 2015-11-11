7dd54f22cf40e0b645a4babbca2471660ac38c71
Han Heul Jo <hjo8@illinois.edu>
2015-11-11 01:51:05 -0600

    connected to firebase, not secure yet

--- database.js
+++ database.js
@@ -1,8 +1,45 @@
-var firebaseRef = new Firebase('https://hackerboard.firebaseio.com/');
+var ref = new Firebase('https://hackerboard.firebaseio.com/');
+var usersRef = ref.child("users");
 
-function push_punchcard(id, punchcard) { //pushes to firebase db no matter what, change to not push if data not changed according to github api
-	firebaseRef.push({
-		username: id,
+function push_punchcard(id, punchcard) {
+	var userRef = usersRef.child(id);
+	userRef.update({
 		punchcard: punchcard
+	});
+}
+
+function push_files(id, files) {
+	var userRef = usersRef.child(id);
+	userRef.update({
+		files: files
+	});
+}
+
+function get_punchcard(id) {
+	var userRef = usersRef.child(id);
+	userRef.on("value", function(snapshot) {
+		outputPunchcard(snapshot.val().punchcard);
+	});
+}
+
+function get_files(id) {
+	var userRef = usersRef.child(id);
+	userRef.on("value", function(snapshot) {
+		outputFiles(snapshot.val().files);
+	});
+}
+
+function set_last_mod(id, time) {
+	var userRef = usersRef.child(id);
+	userRef.update({
+		last_modified: time
+	});
+}
+
+function get_last_mod(id) {
+	var userRef = usersRef.child(id);
+	userRef.on("value", function(snapshot) {
+		console.log(snapshot.val().last_modified);
+		return snapshot.val().last_modified;
 	});
 }
--- github.js
+++ github.js
@@ -1,13 +1,17 @@
+var ref = new Firebase('https://hackerboard.firebaseio.com/');
+var usersRef = ref.child("users");
+
 $(function() {
 	$('#ghsubmitbtn').on('click', function(e){
 		clearPage();
 		e.preventDefault();
 
 		var username = $('#ghusername').val();
-		var requri   = 'https://api.github.com/users/'+username;
-		var repouri  = 'https://api.github.com/users/'+username+'/repos';
 
-		requestJSON(requri, function(json) {
+		var requri   = 'https://api.github.com/users/'+username;
+		var repouri  = 'https://api.github.com/users/'+username + '/repos';
+
+		$.getJSON(requri, function(json, status, xhr) {
 			if(json.message == "Not Found" || username == '') {
 				$('#ghapidata').html("<h2>No User Info Found</h2>");
 			} else {
@@ -21,91 +25,118 @@ $(function() {
 				var followingnum = json.following;
 				var reposnum     = json.public_repos;
 
-				if(fullname == undefined) { fullname = username; }
+				userRef = usersRef.child(username);
+				var last_mod_db;
 
-				var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
-				outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
-				outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
-				outhtml = outhtml + '<div class="repolist clearfix">';
+				userRef.on("value", function(snapshot) {
+					last_mod_db = snapshot.val().last_modified;
 
-				var files = [];
-				var repositories;
-				var trees;
-				var num_done = 0;
-				var punch_done = 0;
-				var punchcard = [];
+					var last_mod_api = xhr.getResponseHeader('Last-Modified');
 
-				$.getJSON(repouri, function (json){
-					repositories = json;
-					var repo_length = repositories.length;
-					//console.log(repo_length);
-					//console.log(repositories);
-					$.each(json, function (rl) {
-						//console.log(rl);
-						var refsuri = 'https://api.github.com/repos/'+username+'/'+repositories[rl].name+'/git/refs/heads';
-						var sha;
-						if (json[rl].size != 0) {
-							$.getJSON(refsuri, function (json) {
-								if (json && json.message != "Not Found" && json.message != "Git Repository is empty.") {
-									sha = json[0].object.sha;
-									var treesuri = 'https://api.github.com/repos/'+username+'/'+repositories[rl].name+'/git/trees/'+sha+'?recursive=1';
-									$.getJSON(treesuri, function (json) {
-										if (json.message != "Not Found") {
-											trees = json.tree;
-											for (i = 0; i < trees.length; i++) {
-												if (trees[i].size != undefined) {
-													files.push(trees[i]);
+					if (last_mod_db === undefined || last_mod_db != last_mod_api) {
+						set_last_mod(username, last_mod_api);
+						console.log("using up thos api calls bruh");
+						var files = [];
+						var repositories;
+						var trees;
+						var num_done = 0;
+						var punch_done = 0;
+						var punchcard = [];
+
+						if (fullname == undefined) { fullname = username; }
+
+						var outhtml = '<h2>'+fullname+' <span class="smallname">(@<a href="'+profileurl+'" target="_blank">'+username+'</a>)</span></h2>';
+						outhtml = outhtml + '<div class="ghcontent"><div class="avi"><a href="'+profileurl+'" target="_blank"><img src="'+aviurl+'" width="80" height="80" alt="'+username+'"></a></div>';
+						outhtml = outhtml + '<p>Followers: '+followersnum+' - Following: '+followingnum+'<br>Repos: '+reposnum+'</p></div>';
+						outhtml = outhtml + '<div class="repolist clearfix">';
+
+						$.getJSON(repouri, function (json){		// add if changed call (304)
+							repositories = json;
+							var repo_length = repositories.length;
+							//console.log(repo_length);
+							//console.log(repositories);
+							$.each(json, function (rl) {
+								//console.log(rl);
+								var refsuri = 'https://api.github.com/repos/'+username+'/'+repositories[rl].name+'/git/refs/heads';
+								var sha;
+								if (json[rl].size != 0) {
+									$.getJSON(refsuri, function (json) {
+										if (json && json.message != "Not Found" && json.message != "Git Repository is empty.") {
+											sha = json[0].object.sha;
+											var treesuri = 'https://api.github.com/repos/'+username+'/'+repositories[rl].name+'/git/trees/'+sha+'?recursive=1';
+											$.getJSON(treesuri, function (json) {
+												if (json.message != "Not Found") {
+													trees = json.tree;
+													for (i = 0; i < trees.length; i++) {
+														if (trees[i].size != undefined) {
+															files.push(trees[i]);
+														}
+													}
+												}
+											}).done(function () {
+											//console.log(files);
+											num_done++;
+											//console.log(num_done);
+											if (repo_length < 10) {  // go ahead when less than 10 repos
+												if (num_done == repo_length) {    // all done code here, or up to certain number of repos (10 at the moment, unordered?)
+													//console.log(num_done);
+													outputFiles(files);
+													push_files(username, files);
+												}
+											} else {
+												console.log(num_done);
+												if (num_done == 10) {  // limits the number of repos
+													outputFiles(files);
+													push_files(username, files);
 												}
 											}
+											});
 										}
-									}).done(function () {
-									//console.log(files);
-									num_done++;
-									//console.log(num_done);
-									if (repo_length < 10) {  // go ahead when less than 10 repos
-										if (num_done == repo_length) {    // all done code here, or up to certain number of repos (10 at the moment, unordered?)
-											//console.log(num_done);
-											outputFiles(files);
-										}
-									} else {
-										console.log(num_done);
-										if (num_done == 10) {  // limits the number of repos
-											outputFiles(files);
-										}
-									}
 									});
+								} else {
+									repo_length--;
 								}
-							});
-						} else {
-							repo_length--;
-						}
-						var punchuri = 'https://api.github.com/repos/'+username+'/'+repositories[rl].name+'/stats/punch_card';
-						$.getJSON(punchuri, function (json) {
-							if (json && json.message != "Not Found") {
-								for (i = 0; i < json.length; i++) {
-									if (json[i][2] != 0 ) {
-										punchcard.push(json[i]);
+
+								var punchuri = 'https://api.github.com/repos/'+username+'/'+repositories[rl].name+'/stats/punch_card';
+
+								$.getJSON(punchuri, function (json) {		// add if changed (304)
+									if (json && json.message != "Not Found") {
+										for (i = 0; i < json.length; i++) {
+											if (json[i][2] != 0 ) {
+												punchcard.push(json[i]);
+											}
+										}
 									}
-								}
-							}
-						}).done(function () {
-							punch_done++;
-							if (punch_done == repositories.length) {
-								outputPunchcard(punchcard);
-								push_punchcard(username, punchcard);
-							}
+								}).done(function () {
+									punch_done++;
+									if (punch_done == repositories.length) {
+										outputPunchcard(punchcard);
+										push_punchcard(username, punchcard);
+									}
+								});
+							});
 						});
-					});
+					} else {
+						console.log("not using more api calls for this mofo");
+						get_punchcard(username);
+						get_files(username);
+					}
 				});
-			} // end of else
+			}
 		}); // end requestJSON Ajax call
 	}); // end click event handler
 
-	function requestJSON(url, callback) {
+	function requestJSON(url, time, callback) {
 		$.ajax({
 			url: url,
+			headers: { 'If-Modified-Since': time },
+			beforeSend: function(xhr) {
+				xhr.setRequestHeader('If-Modified-Since', time);
+				console.log(xhr);
+				console.log(xhr.getAllResponseHeaders());
+			},
 			complete: function(xhr) {
-				callback.call(null, xhr.responseJSON);
+				callback.call(null, xhr);
 			}
 		});
 	}
